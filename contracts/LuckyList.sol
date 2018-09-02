2f58df06402dd7cf6546b0833856e6d04530add7
Alexander Mangel <cygnusfear@gmail.com>
2018-09-02 14:50:36 +0200

    contract now actually works yay

--- contracts/LuckyList.sol
+++ contracts/LuckyList.sol
@@ -6,11 +6,12 @@ contract LuckyList {
     
     // You know, at some point we'll do some fancy curved token bonding here
     address owner;
-    uint price = 0;//10000000000000000;
-    uint list_max = 25;
+    uint price = 40000000000000000;
+    uint list_max = 100;
 
     uint current_index = 0;
-    address[] keys = new address[](25);
+    bool wrapped = false;
+    address[] keys = new address[](100);
     mapping(address => Lucky) luckyByAddress; 
 
     constructor() public {
@@ -21,31 +22,34 @@ contract LuckyList {
         address _address;
         string _name;
         string _logo_ipfs;
-    }    
-
+    }  
+    
     // Add yourself to Ze List Of AweZome! (buy the Dank Mono Font by the way, disclaimer: I am just a user of the font but it has the most porno Z of all programming fonts in existenZ)
-    function addAddress(string _name, string _logo_ipfs) public payable returns(string) {
-        if (msg.value < price) return "Couldn't add because you underpaid";
-        Lucky memory newLucky = Lucky(msg.sender, _name, _logo_ipfs);
+    function addAddress(address _address, string _name, string _logo_ipfs) public payable returns(string) {
+        if (owner != msg.sender)
+        {
+            require(msg.value >= price, "Couldn't add because you underpaid");
+        }
+        else
+        {
+            price += 40000000000000000;
+        }
+        Lucky memory newLucky = Lucky(_address, _name, _logo_ipfs);
         //we have a new lucky
-        luckyByAddress[msg.sender] = newLucky;
+        luckyByAddress[_address] = newLucky;
         //add address to keys LUT
-        keys[current_index] = msg.sender;
+        keys[current_index] = _address;
+        price += 40000000000000000;
         //increase the indexer
         current_index += 1;
-        //and we 
-        // price += 10000000000000000;
-        if (current_index > list_max)
+        if (current_index >= list_max)
         {
             current_index = 0;
+            wrapped = true;
         }
-        if (keys[current_index] != 0)
-        {
-            delete luckyByAddress[keys[current_index]];
-            delete keys[current_index];   
-        }
-        return "You are on the lucky list!";
+        return "You are now on the lucky list!";
     }
+    
 
     // This is to let the owner remove information about his address that he doesn't like, receives a small refund for his efforts and the sad fact that he's not on the list anymore
     function getMeOffTheFuckingList() public {
@@ -57,19 +61,34 @@ contract LuckyList {
     // This is for me, all of it, it funds my habits
     function lootDonationBox() public
     {
-        if (msg.sender != owner) return;
+        assert(msg.sender == owner);
         msg.sender.transfer(address(this).balance);
     }
     
     function getLucky(uint i) public view returns(address, string, string) 
     {
-        Lucky memory temp = luckyByAddress[keys[i]];
-        return (temp._address, temp._name, temp._logo_ipfs);
+        return (luckyByAddress[keys[i]]._address, luckyByAddress[keys[i]]._name, luckyByAddress[keys[i]]._logo_ipfs);
     }
     
-    function getIndex() public view returns(uint) 
+    function getAddress(address a) public view returns(address, string, string) 
     {
-        return current_index;
+        return (luckyByAddress[a]._address, luckyByAddress[a]._name, luckyByAddress[a]._logo_ipfs);
+    }
+    
+    function setPrice(uint _price) public
+    {
+        assert(msg.sender == owner);
+        price = _price;
+    }
+    
+    function getIndexAndLength() public view returns(uint, bool) 
+    {
+        return (current_index, wrapped);
+    }
+    
+    function getPrice() public view returns(uint)
+    {
+        return price;
     }
     
 }
